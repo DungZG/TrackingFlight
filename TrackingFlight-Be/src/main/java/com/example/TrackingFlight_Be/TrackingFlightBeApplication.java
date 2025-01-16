dc1dd5099e0c105c78c81e846f37b43210ed0989
Dungnt <dungzg1201@gmail.com>
2025-01-16 09:48:21 +0700

    first-commit

---
+++ TrackingFlight-Be/src/main/java/com/example/TrackingFlight_Be/TrackingFlightBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrackingFlight_Be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingFlightBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingFlightBeApplication.class, args);
+	}
+
+}
---
+++ TrackingFlight-Be/src/test/java/com/example/TrackingFlight_Be/TrackingFlightBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrackingFlight_Be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingFlightBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
