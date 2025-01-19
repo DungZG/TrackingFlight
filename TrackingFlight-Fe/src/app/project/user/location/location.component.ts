68a3648e942ad219b067af381083772aaba44922
Dungnt <dungzg1201@gmail.com>
2025-01-19 16:15:50 +0700

    update

--- TrackingFlight-Fe/src/app/project/user/location/location.component.ts
+++ TrackingFlight-Fe/src/app/project/user/location/location.component.ts
@@ -1,5 +1,5 @@
+import { FormGroup } from '@angular/forms';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-location',
   standalone: false,
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './location.component.scss'
 })
 export class LocationComponent {
-
+  formGroup!: FormGroup;
+  constructor(
+    
+  ){}
 }
