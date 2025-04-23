ec06188bb97728d6e443c8e9c7909145ae09c41b
thuctap-dungnt <tridung4323@gmail.com>
2025-04-23 14:42:19 +0700

    update

---
+++ TrackingFlight-Be/src/main/java/com/example/TrackingFlight_Be/v1/repositories/SeatsRepository.java
@@ -0,0 +1,12 @@
+package com.example.TrackingFlight_Be.v1.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SeatsRepository {
+}
